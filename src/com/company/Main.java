ebce1241f93ed096469f857f4ca599eea78c234a
Matthew Menna <mattmenna@gmail.com>
2017-05-22 14:17:51 -0400

    Added Time classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,9 +12,16 @@ public class Main {
 
         System.out.println("Duration Between Dates");
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter the date in Years, Months and Days all numbers");
+
+        int year = s.nextInt();
+        int month = s.nextInt();
+        int daysOfmonth = s.nextInt();
 
         LocalDate today = LocalDate.now();
-        LocalDate tomorrow = LocalDate.of(2018, Month.APRIL, 14);
+
+        LocalDate tomorrow = LocalDate.of(year, Month.of(month), daysOfmonth);
+
         Period p = Period.between(today, tomorrow);
         System.out.println("Number of Years "+ p.getYears() + " Number of Months " + p.getMonths() + " Number of Days " + p.getDays());
 
