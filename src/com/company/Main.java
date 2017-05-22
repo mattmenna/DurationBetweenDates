1bb5c3aa7eac3f8f97dc72730b4864c5f163e141
Matthew Menna <mattmenna@gmail.com>
2017-05-22 14:54:45 -0400

    Regex work and string breakdown

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,36 @@ public class Main {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the date in Years, Months and Days all numbers");
 
+        String entry1 = s.nextLine();
+
+
+        if (entry1.matches("\\d\\d\\W\\d\\d\\W\\d\\d\\d\\d") == false) {
+            System.out.println("Wrong format");
+            return;
+        }
+       String monthString = entry1.substring(0,1);
+        int monthInt = Integer.valueOf(monthString);
+
+        String dayString = entry1.substring(3,4);
+        int dayInt = Integer.valueOf(monthString);
+
+        String yearString = entry1.substring(6,9);
+        int yearInt = Integer.valueOf(monthString);
+
+        LocalDate firstdate = LocalDate.of(yearInt, Month.of(monthInt), dayInt);
+
+        String entry2 = s.nextLine();
+
+
+        if (entry2.matches("\\d\\d\\W\\d\\d\\W\\d\\d\\d\\d") == false) {
+            System.out.println("Wrong format");
+            return;
+        }
+
+
+        //LocalDate secondDate = LocalDate.of(year, Month.of(month), daysOfmonth);
+
+    /*
         int year = s.nextInt();
         int month = s.nextInt();
         int daysOfmonth = s.nextInt();
@@ -23,8 +53,8 @@ public class Main {
         LocalDate tomorrow = LocalDate.of(year, Month.of(month), daysOfmonth);
 
         Period p = Period.between(today, tomorrow);
-        System.out.println("Number of Years "+ p.getYears() + " Number of Months " + p.getMonths() + " Number of Days " + p.getDays());
-
+        System.out.println("Number of Years " + p.getYears() + " Number of Months " + p.getMonths() + " Number of Days " + p.getDays());
+    */
 
     }
 }
